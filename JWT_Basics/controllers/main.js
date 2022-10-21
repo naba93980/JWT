201a3987e73799def91a70c7b555edbb589b3e1d
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-22 04:47:57 +0530

    login controller creates token, dashboard controller sends responds once token is verified in the auth middleware

--- JWT_Basics/controllers/main.js
+++ JWT_Basics/controllers/main.js
@@ -1,17 +1,23 @@
-const CustomAPIError = require('../errors/custom-error');
+const {BadRequest} = require("../errors");
+const jwt = require("jsonwebtoken");
 
 const login = async (req, res) => {
     const { username, password } = req.body;
     if (!username || !password) {
-        throw new CustomAPIError('please provide email and password', 400);
+        throw new BadRequest('please provide email and password');
     }
-    res.send("Fake Login/Register/Signup Route");
 
+    //just for demo, normally provided by DB!!!!
+    const id = new Date().getDate();
+
+    const token = jwt.sign({ id, username }, process.env.JWT_TOKEN, { expiresIn: '30s' })
+    res.status(200).json({ mssg: "user logged", token });
 };
+
 
 const dashboard = async (req, res) => {
     const luckyNumber = Math.floor(Math.random() * 100);
-    res.status(200).json({ msg: "Hello, John Doe", secret: `Here is your authorized data, r lucky number is ${luckyNumber}` });
+    res.status(200).json({ msg: `Hello, ${req.user.username}`, secret: `Here is your authorized data, r lucky number is ${luckyNumber}` });
 };
 
 module.exports = {
