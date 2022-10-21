11939886b67a51a5fc3c5a4b4d984876f7a3bb78
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-22 04:48:21 +0530

    contains all custom classes together

---
+++ JWT_Basics/errors/index.js
@@ -0,0 +1,9 @@
+const CustomAPIError = require('./custom-error');
+const BadRequest = require('./bad-request');
+const UnauthorizationError = require("./Unauthorization");
+
+module.exports = {
+  CustomAPIError,
+  BadRequest,
+  UnauthorizationError,
+};
