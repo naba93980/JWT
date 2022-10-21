8b6d2d1e5414813fa6bd991e319f9cc279d117a3
naba93980 <72063612+naba93980@users.noreply.github.com>
2022-10-22 04:45:03 +0530

    custom error classes

--- JWT_Basics/errors/custom-error.js
+++ JWT_Basics/errors/custom-error.js
@@ -1,8 +1,6 @@
 class CustomAPIError extends Error {
-  constructor(message, statusCode) {
+  constructor(message) {
     super(message)
-    this.statusCode = statusCode
   }
 }
-
 module.exports = CustomAPIError
